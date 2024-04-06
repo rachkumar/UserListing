594b29f2377f61fa2e4a9f2b27b4f9837445d1ae
rachkumar <rachkumr77@gmail.com>
2024-04-06 15:33:58 +0530

    Initial Commit

---
+++ Userlist/UserlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserlistApp.swift
+//  Userlist
+//
+//  Created by Raj Kumar on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UserlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
